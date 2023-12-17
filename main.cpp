07df6113652f25b027bd919695b9befe6a40c9c8
conrev <andrewmahisa@gmail.com>
2023-12-17 11:29:18 +1100

    feat: render the first triangle!

--- main.cpp
+++ main.cpp
@@ -1,39 +1,222 @@
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
+#include <memory>
 #include <iostream>
+
+SDL_Window *gWindow = NULL;
+SDL_GLContext gGLContext = NULL;
+bool gQuit = false;
 
 int SCREEN_WIDTH = 800;
 int SCREEN_HEIGHT = 640;
+unsigned int VBO, VAO, shaderProgram;
 
-int main()
+GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+
+};
+
+bool setupShader();
+
+bool init()
 {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         std::cout << "Failed to initialize the SDL2 library\n";
-        return -1;
+        return false;
     }
 
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
-                                          0);
+    gWindow = SDL_CreateWindow("SDL2 Window",
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SCREEN_WIDTH, SCREEN_HEIGHT,
+                               SDL_WINDOW_OPENGL);
 
-    if (!window)
+    if (!gWindow)
     {
-        std::cout << "Failed to create window\n";
-        return -1;
+        std::cout << "Failed to create SDL Window";
+        return false;
     }
 
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    gGLContext = SDL_GL_CreateContext(gWindow);
 
-    if (!window_surface)
+    if (!gGLContext)
     {
-        std::cout << "Failed to get the surface from the window\n";
-        return -1;
+        std::cout << "Failed to Create GL Context";
+        return false;
     }
 
-    SDL_UpdateWindowSurface(window);
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }
+    // initialize glViewport
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    SDL_Delay(5000);
+    // setupShader
+    if (!setupShader())
+    {
+        std::cout << "Failed to setup Shader" << std::endl;
+        return false;
+    };
+
+    return true;
+}
+
+void input()
+{
+    SDL_Event evt;
+
+    while (SDL_PollEvent(&evt) != 0)
+    {
+        if (evt.type == SDL_QUIT)
+        {
+            gQuit = true;
+        }
+    }
+}
+
+bool setupShader()
+{
+
+    // shader debuggers
+    int success;
+    char infoLog[512];
+
+    const char *vertexShaderSource = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+        return success;
+    }
+
+    const char *fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 FragColor;\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\n";
+
+    unsigned int fragmentShader;
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+        return success;
+    }
+
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return true;
+}
+
+void preDraw()
+{
+    // VAO stores pointers to VBO to potentially swap many different VBOs
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // bind the vertex array
+    glBindVertexArray(VAO);
+    // The array buffer is now bounded to our VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // the VBO now has data, our vertices array
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // tell opengl how to interpret our bounded array buffer, aka the VBO
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void draw()
+{
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_POINTS, 0, 3);
+}
+
+void engineLoop()
+{
+
+    while (!gQuit)
+    {
+        input();
+
+        preDraw();
+
+        draw();
+        // Swap front to back buffer
+        SDL_GL_SwapWindow(gWindow);
+    }
+}
+
+void cleanUp()
+{
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    SDL_DestroyWindow(gWindow);
+
+    SDL_Quit();
+}
+
+int main()
+{
+    int initState = init();
+
+    std::cout << initState;
+
+    if (!initState)
+        return 1;
+
+    engineLoop();
+    cleanUp();
 }
