cec0c722072f6bad7d9d1caf566cbc5cd39a2b81
conrev <andrewmahisa@gmail.com>
2023-12-18 13:11:40 +1100

    feat: implement first EBO

--- main.cpp
+++ main.cpp
@@ -11,17 +11,25 @@ bool gQuit = false;
 
 int SCREEN_WIDTH = 800;
 int SCREEN_HEIGHT = 640;
-unsigned int pVBO, cVBO, VAO, shaderProgram;
+unsigned int VBO, VAO, EBO, shaderProgram;
 
 GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f};
+    -0.5f, -0.5f, 0.0f, // p0
+    1.0f, 0.0f, 0.0f,   // c0
+    0.5f, -0.5f, 0.0f,  // p1
+    0.0f, 1.0f, 0.0f,   // c1
+    -0.5f, 0.5f, 0.0f,  // p2
+    0.0f, 0.0f, 1.0f,   // c2
+    0.5f, 0.5f, 0.0f,   // p3
+    0.0f, 0.0f, 1.0f    // c3
 
-GLfloat colors[] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f};
+};
+
+GLuint indices[] = {
+    // note that we start from 0!
+    0, 1, 2, // first triangle
+    1, 3, 2  // second triangle
+};
 
 bool setupShader();
 std::string loadShaderFromFile(const std::string &filename)
@@ -45,7 +53,7 @@ std::string loadShaderFromFile(const std::string &filename)
 bool init()
 {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -83,6 +91,7 @@ bool init()
     }
     // initialize glViewport
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glEnable(GL_CULL_FACE);
 
     // setupShader
     if (!setupShader())
@@ -114,7 +123,7 @@ bool setupShader()
     int success;
     char infoLog[512];
 
-    std::string vertexShaderSource = loadShaderFromFile("../shaders/vert.glsl");
+    std::string vertexShaderSource = loadShaderFromFile("./shaders/vert.glsl");
     const char *vertexShaderSrc = vertexShaderSource.c_str();
 
     unsigned int vertexShader;
@@ -133,7 +142,7 @@ bool setupShader()
         return success;
     }
 
-    std::string fragmentShaderSource = loadShaderFromFile("../shaders/frag.glsl");
+    std::string fragmentShaderSource = loadShaderFromFile("./shaders/frag.glsl");
     const char *fragmentShaderSrc = fragmentShaderSource.c_str();
 
     unsigned int fragmentShader;
@@ -168,29 +177,29 @@ void preDraw()
 {
     // VAO stores pointers to VBO to potentially swap many different VBOs
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &pVBO);
-    glGenBuffers(1, &cVBO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // bind the vertex array
     glBindVertexArray(VAO);
     // The array buffer is now bounded to our VBO
-    glBindBuffer(GL_ARRAY_BUFFER, pVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     // the VBO now has data, our vertices array
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // bind array element buffer to our EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    // put the data in
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-
-    // setup second vbo for colors
-    glBindBuffer(GL_ARRAY_BUFFER, cVBO);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     // tell opengl how to interpret our bounded array buffer, aka the VBO
 
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -204,7 +213,7 @@ void draw()
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 void engineLoop()
@@ -226,8 +235,7 @@ void cleanUp()
 {
 
     glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &pVBO);
-    glDeleteBuffers(1, &cVBO);
+    glDeleteBuffers(1, &VBO);
 
     glDeleteProgram(shaderProgram);
 
@@ -240,11 +248,11 @@ int main()
 {
     int initState = init();
 
-    std::cout << initState;
-
     if (!initState)
         return 1;
 
     engineLoop();
     cleanUp();
+
+    return 0;
 }
